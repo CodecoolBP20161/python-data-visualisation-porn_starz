5f48b402de72e3833bc10022cab17d4a707f905d
TBalint19 <toth910719balint@gmail.com>
2016-07-07 12:37:44 +0200

    perszehogyjo2

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ from sqlhandling import Sql
 from text import Text
 
 
-picture = Image.new("RGB", (1024, 1024), "gold")
+picture = Image.new("RGB", (1024, 1024), "white")
 
-Sql.openDataBase('csibi', 'postgres', 'localhost', 'csibi')
+Sql.openDataBase('TothBalint', 'postgres', 'localhost', 'Fuzzwktrka88bcykjypt')
 Sql.writeQuery(''' SELECT company_name as "Companies", count(company_name) as "Projects" FROM project
 GROUP BY company_name ORDER BY count(company_name) DESC''')
 first_tuple_list = Sql.getData()
@@ -27,15 +27,25 @@ for color_list in first_list: # [company_name, projects,  [(hex1,), (hex2)...]]
         g += color_list[2][i][1]
         b += color_list[2][i][2]
     if color_list[2] != []:
-        r = r/len(color_list[2])
-        g = g / len(color_list[2])
-        b = b / len(color_list[2])
+        r = int(r/len(color_list[2]))
+        g = int(g / len(color_list[2]))
+        b = int(b / len(color_list[2]))
         color_list[2] = [r, g, b]
     else:
         continue
 
 for text_info in first_list:
-    text = Text(text_info[0], text_info[1]*3, tuple(text_info[2]), "Capture_it.ttf", 255)
+    text = Text(text_info[0], text_info[1]*5, tuple(text_info[2]), "Fonts/Capture_it.ttf", 255)
 
+for text in Text.text_object_list:
+    text.calculate_length(picture)
+
+Text.order_list()
+for text in Text.ordered_list:
+    text.findPlace()
+
+for text in Text.ordered_list:
+    picture = text.place(picture)
+picture.save("perszehogyjo.png")
 
 
